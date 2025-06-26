fb9d27e1cf673f49ab4ac03bc15e9051edd48dcb
danilovukosich <vukosichdanilo@gmail.com>
2025-06-26 15:48:48 -0300

    avances antes de integrar pdfs

--- src/app/components/layouts/modals/ver-historia-clinica/ver-historia-clinica.component.ts
+++ src/app/components/layouts/modals/ver-historia-clinica/ver-historia-clinica.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ver-historia-clinica',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ver-historia-clinica.component.html',
   styleUrl: './ver-historia-clinica.component.css'
 })
 export class VerHistoriaClinicaComponent {
+
+
+
+
+  async ngOnInit() 
+  {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
 
 }
